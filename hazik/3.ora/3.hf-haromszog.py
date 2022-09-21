555732444d69c1449a5ac4780df010e59d6f95a4
arrora09 <ronyka1009@gmail.com>
2022-09-21 21:18:10 +0200

    hazik

---
+++ hazik/3.ora/3.hf-haromszog.py
@@ -0,0 +1,15 @@
+
+def vizsgalat(a,b,c):
+    if a+b>c and a+c>b and b+c>a:
+        print("A {}, {} és {} oldalú háromszög szerkeszthető.".format(a,b,c))
+    else:
+        print("A {}, {} és {} oldalú háromszög NEM szerkeszthető.".format(a, b, c))
+
+
+if __name__=="__main__":
+    print("Adja meg a háromszög három oldalát cm-ben:")
+    a = int(input("a oldal (cm): "))
+    b = int(input("b oldal (cm): "))
+    c = int(input("c oldal (cm): "))
+
+    vizsgalat(a,b,c)
